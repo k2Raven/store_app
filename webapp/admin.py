bbb78355f1215e8b1fd85ad45d0d17224f49e3c9
Aleksei_Kuzmin <leha.21.05.1996@gmail.com>
2022-11-16 19:46:36 +0600

    Зарегистрировал модель в админ-панели

--- webapp/admin.py
+++ webapp/admin.py
@@ -1,3 +1,13 @@
 from django.contrib import admin
+from webapp.models import Product
+
 
 # Register your models here.
+class ProductAdmin(admin.ModelAdmin):
+    list_display = ['id', 'name', 'category', 'balance', 'price']
+    list_filter = ['category']
+    search_fields = ['name', 'description']
+    exclude = []
+
+
+admin.site.register(Product, ProductAdmin)
