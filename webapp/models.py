8fdfd08d2543bba979684eda47e6a4af780c167c
Aleksei_Kuzmin <leha.21.05.1996@gmail.com>
2022-11-16 19:36:32 +0600

    создал модель "Товар" (Product)

--- webapp/models.py
+++ webapp/models.py
@@ -1,3 +1,17 @@
 from django.db import models
 
+CATEGORIES_CHOICES = [("other", "Разное"), ("electronics", "Электроника"), ("books", "Книги"),
+                      ("stationery", "Канцтовары")]
+
+
 # Create your models here.
+class Product(models.Model):
+    name = models.CharField(max_length=100, null=False, blank=False, verbose_name='Наименование товара')
+    description = models.TextField(max_length=2000, null=True, blank=True, verbose_name='Описание товара')
+    category = models.CharField(max_length=50, choices=CATEGORIES_CHOICES, default=CATEGORIES_CHOICES[0][0],
+                                verbose_name="Категория")
+    balance = models.PositiveIntegerField(verbose_name='Остаток')
+    price = models.DecimalField(max_digits=7, decimal_places=2, verbose_name="Стоимость")
+
+    def __str__(self):
+        return f'{self.id}. {self.name}'
