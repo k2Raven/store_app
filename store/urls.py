b5780ded10a1b4f6f87fbadd4c107856d11b6adf
Aleksei_Kuzmin <leha.21.05.1996@gmail.com>
2022-11-16 20:29:22 +0600

    Создал страницы просмотра товара: Список товаров, Просмотр одного товара по id

--- store/urls.py
+++ store/urls.py
@@ -15,7 +15,11 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
+from webapp.views import index_view, product_view
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('', index_view, name='index'),
+    path('product/<int:pk>/', product_view, name='product_view')
+
 ]
--- webapp/views.py
+++ webapp/views.py
@@ -1,3 +1,13 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
+from webapp.models import Product
+
 
 # Create your views here.
+def index_view(request):
+    products = Product.objects.filter(balance__gt=0).order_by('category', 'name')
+    return render(request, 'index.html', {'products': products})
+
+
+def product_view(request, pk):
+    product = get_object_or_404(Product, pk=pk)
+    return render(request, 'product_view.html', {'product': product})
