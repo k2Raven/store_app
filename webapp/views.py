98b333eaed526302e7ceca90e764c2df8db6a5aa
Aleksei_Kuzmin <leha.21.05.1996@gmail.com>
2022-11-16 20:58:39 +0600

    Добавил поиск товара по названию

--- webapp/views.py
+++ webapp/views.py
@@ -1,11 +1,19 @@
 from django.shortcuts import render, get_object_or_404
 from webapp.models import Product
+from webapp.forms import SearchForm
 
 
 # Create your views here.
 def index_view(request):
     products = Product.objects.filter(balance__gt=0).order_by('category', 'name')
-    return render(request, 'index.html', {'products': products})
+    form = SearchForm(data=request.GET)
+    if form.is_valid():
+        name = form.cleaned_data['search']
+        if name:
+            products = products.filter(name=name)
+            # products = products.filter(name__icontains=name)
+
+    return render(request, 'index.html', {'products': products, 'form': form})
 
 
 def product_view(request, pk):
